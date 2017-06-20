9908585150b48ca4bf17fe7de086bf024f4e7831
Sky Link <s.z_link@yahoo.com>
2017-06-20 12:03:43 -0600

    Password generator

---
+++ src/edu/cnm/deepdive/security/PasswordGenerator.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive.security;
+
+/**
+ * Implementation of a simple password generator.
+ * 
+ * @author Sky Link
+ * @version 1.0
+ */
+public class PasswordGenerator {
+  
+  /**
+   * Instantiate PasswordGenerator class for testing password generation.
+   * 
+   * @param args Command-line parameters for password generation options.
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
