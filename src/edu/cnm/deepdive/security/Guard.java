935d8d8ae7d81c6f9008ec93d7a353e34141bbc2
Sky Link <s.z_link@yahoo.com>
2017-06-22 13:59:26 -0600

    CommandLine objects

--- src/edu/cnm/deepdive/security/Guard.java
+++ src/edu/cnm/deepdive/security/Guard.java
@@ -3,6 +3,7 @@
  */
 package edu.cnm.deepdive.security;
 
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -26,36 +27,41 @@ public class Guard {
    * @param args  Command line arguments, specifying generation options.
    */
   public static void main(String[] args) {
-   Option[] options = getOptions(args);
-   String artifact = generateArtifact(options);
+   CommandLine cmdLine = getOptions(args);
+   String artifact = generateArtifact(cmdLine);
    emitArtifact(artifact);
   }
   
-  static Option[] getOptions(String[] args) {
+  static CommandLine getOptions(String[] args) {
     
     try {
       
-      Option lengthOption = Option.builder("L").argName("length")
+      Option lengthOption = Option.builder("k").argName("length")
                                                .hasArg()
                                                .longOpt("length")
                                                .numberOfArgs(1)
+                                               .type(Integer.class)
                                                .build();
       Option delimiterOption = Option.builder("d").argName("delimiter")
                                                  .hasArg()
                                                  .longOpt("delimiter")
                                                  .numberOfArgs(1)
                                                  .optionalArg(true)
+                                                 .type(String.class)
                                                  .build();
       Option wordListOption = Option.builder("w").argName("path-to-list-file")
                                                  .hasArg()
                                                  .longOpt("word-list")
                                                  .numberOfArgs(1)
+                                                 .type(String.class)
                                                  .build();
       Options options = new Options().addOption(lengthOption)
                                      .addOption(delimiterOption)
                                      .addOption(wordListOption);
       DefaultParser parser = new DefaultParser();
-      return parser.parse(options, args).getOptions();
+      CommandLine cmdLine = parser.parse(options, args);
+      Object test = cmdLine.getParsedOptionValue("k"); // FIXME - debug statement
+      return cmdLine;
       
     } catch (ParseException ex) {
       // TODO handle this exception with a usage display.
@@ -64,7 +70,7 @@ public class Guard {
 
   }
   
-  static String generateArtifact(Option[] options) {
+  static String generateArtifact(CommandLine cmdLine) {
     return null;// FIXME
   }
   
